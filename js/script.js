c88accf0a01831036b7f52f5b37d1a55a3a66cf1
Eviomarcio <eviomarcio.silva@gmail.com>
2020-07-22 20:34:55 -0300

    Criação da função de delete

--- js/script.js
+++ js/script.js
@@ -42,25 +42,40 @@ function render(){
     var divNames = document.querySelector('#names');
     divNames.innerHTML = '';
     var ul = document.createElement('ul');
-
-    for(var i; i < globalNames.length; i++){
+    
+    for(var i = 0; i < globalNames.length; i++){
         var currentName = globalNames[i];
 
         var li = document.createElement('li');
-
-        var button = document.createElement('button');
-        button.textContent = 'x';
+        var button = createDeleteButton(i);
+        
 
         var span = document.createElement('span');
         span.textContent = currentName;
 
         li.appendChild(button);
         li.appendChild(span);
+
         ul.appendChild(li);
     }
 
     divNames.appendChild(ul);
     clearInput();
+
+    function createDeleteButton(index){
+        function deleteName(){
+            globalNames.splice(index, 1);
+            render();
+        }
+
+        var button = document.createElement('button');
+        button.classList.add('deleteButton');
+        button.textContent = 'x';
+
+        button.addEventListener('click', deleteName);
+
+        return button;
+    }
 }
 
 function clearInput(){
